2785ad7f06fed4a402a7d188e0e1f43f5a1c1ad7
Jesper Falkenby <jesper.falkenby@gmail.com>
2025-10-14 22:40:20 +0200

    add some lorem ipsum text

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,39 @@ export default function Home() {
       <Section headingLevel={1} id="home" title="Jesper Falkenby">
         <div className={subtitle({ class: "mt-4" })}>Full-Stack Developer</div>
       </Section>
-      <Section headingLevel={2} id="resume" title="Résumé" />
-      <Section headingLevel={2} id="publications" title="Publications" />
-      <Section headingLevel={2} id="about" title="About" />
+      <Section headingLevel={2} id="resume" title="Résumé">
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Section>
+      <Section headingLevel={2} id="publications" title="Publications">
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Section>
+      <Section headingLevel={2} id="about" title="About">
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Section>
       <Section headingLevel={2} id="contact" title="Contact" />
     </main>
   );
