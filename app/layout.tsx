7b1f13a67b28af32e4cd4c53a7d2d96e2ea10e36
Jesper Falkenby <jesper.falkenby@gmail.com>
2025-12-07 21:48:10 +0100

    layout styling fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       >
         <AnimatedBackground />
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex flex-col">
             <Navbar />
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
               {children}
