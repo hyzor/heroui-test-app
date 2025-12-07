25de50130239eaab3d2ae5406717e07298193f6e
Jesper Falkenby <jesper.falkenby@gmail.com>
2025-12-07 22:23:45 +0100

    change navbar styling and other fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,18 +5,16 @@ import {
   NavbarContent,
   NavbarMenu,
   NavbarMenuToggle,
-  NavbarBrand,
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
 import { useState } from "react";
 import { Link } from "@heroui/link";
 import { link as linkStyles } from "@heroui/theme";
-import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
-import { GithubIcon, Logo, LinkedInIcon, MailIcon } from "@/components/icons";
+import { GithubIcon, LinkedInIcon, MailIcon } from "@/components/icons";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,86 +30,90 @@ export const Navbar = () => {
   };
 
   return (
-    <HeroUINavbar
-      isMenuOpen={isMenuOpen}
-      maxWidth="xl"
-      position="sticky"
-      onMenuOpenChange={setIsMenuOpen}
-    >
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <a
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                href={item.href}
-                onClick={(e) => handleSmoothScroll(e, item.href)}
-              >
-                {item.label}
-              </a>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
+    <div className="fixed top-0 left-0 right-0 z-50 bg-black/20 backdrop-blur-md">
+      <HeroUINavbar
+        isMenuOpen={isMenuOpen}
+        maxWidth="xl"
+        position="static"
+        onMenuOpenChange={setIsMenuOpen}
+        className="bg-transparent border-none"
+        classNames={{
+          wrapper: "bg-transparent px-4",
+          menu: "bg-black/20",
+          menuItem: "bg-transparent",
+          toggle: "bg-transparent",
+        }}
       >
-        <NavbarItem className="hidden sm:flex gap-2">
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+          <ul className="hidden lg:flex gap-4 justify-start ml-2">
+            {siteConfig.navItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <a
+                  className={clsx(
+                    linkStyles({ color: "foreground" }),
+                    "data-[active=true]:text-primary data-[active=true]:font-medium",
+                  )}
+                  href={item.href}
+                  onClick={(e) => handleSmoothScroll(e, item.href)}
+                >
+                  {item.label}
+                </a>
+              </NavbarItem>
+            ))}
+          </ul>
+        </NavbarContent>
+
+        <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+          <NavbarItem className="hidden sm:flex gap-4">
+            <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+              <GithubIcon className="text-default-500" />
+            </Link>
+            <Link
+              isExternal
+              aria-label="LinkedIn"
+              href={siteConfig.links.linkedin}
+            >
+              <LinkedInIcon className="text-default-500" />
+            </Link>
+            <Link aria-label="Email" href={siteConfig.links.email}>
+              <MailIcon className="text-default-500" />
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
-          </Link>
-          <Link
-            isExternal
-            aria-label="LinkedIn"
-            href={siteConfig.links.linkedin}
-          >
-            <LinkedInIcon className="text-default-500" />
           </Link>
           <Link aria-label="Email" href={siteConfig.links.email}>
             <MailIcon className="text-default-500" />
           </Link>
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarMenuToggle />
+        </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <Link aria-label="Email" href={siteConfig.links.email}>
-          <MailIcon className="text-default-500" />
-        </Link>
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color="foreground"
-                href={item.href}
-                size="lg"
-                onClick={(e) => {
-                  handleSmoothScroll(e as any, item.href);
-                  setIsMenuOpen(false);
-                }}
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </HeroUINavbar>
+        <NavbarMenu>
+          <div className="mx-4 mt-2 flex flex-col gap-2">
+            {siteConfig.navMenuItems.map((item, index) => (
+              <NavbarMenuItem key={`${item}-${index}`}>
+                <Link
+                  color="foreground"
+                  href={item.href}
+                  size="lg"
+                  onClick={(e) => {
+                    handleSmoothScroll(e as any, item.href);
+                    setIsMenuOpen(false);
+                  }}
+                >
+                  {item.label}
+                </Link>
+              </NavbarMenuItem>
+            ))}
+          </div>
+        </NavbarMenu>
+      </HeroUINavbar>
+    </div>
   );
 };
